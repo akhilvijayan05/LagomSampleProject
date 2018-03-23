9d8cba00c3320ca519df0d6f48f59dedb8c3e4ad
akhilvijayan05 <akhilvijayan05@gmail.com>
2018-03-23 13:20:08 +0530

    Added test cases for Consumer class

--- sample-example-impl/src/main/scala/com/example/impl/kafka/Consumer.scala
+++ sample-example-impl/src/main/scala/com/example/impl/kafka/Consumer.scala
@@ -2,8 +2,9 @@ package com.example.impl.kafka
 
 import java.util.{Collections, Properties}
 
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
 
+import scala.annotation.tailrec
 import scala.collection.JavaConversions._
 import scala.io.Source.fromURL
 
@@ -14,16 +15,29 @@ trait Consumer {
   props.load(reader)
   val consumer = new KafkaConsumer[Integer, Integer](props)
 
-  def consumeData(topic: String): Unit = {
+  def consumeData(topic: String): Map[Integer, Integer] = {
 
     consumer.subscribe(Collections.singletonList(topic))
+    val result = consumerPoll(topic, consumer.poll(5000), Map())
+    consumer.close()
+    println("Done")
+    result
+  }
 
-    while (true) {
-      val records = consumer.poll(1000)
+  @tailrec
+  private def consumerPoll(topic: String, records: ConsumerRecords[Integer, Integer], data: Map[Integer, Integer]): Map[Integer, Integer] = {
 
-      for (record <- records) {
+    if (records.count() == 0) {
+      data
+    } else {
+      val record = for {
+        record <- records
+      } yield {
         System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset())
+        record.key() -> record.value()
       }
+      consumerPoll(topic, consumer.poll(1000), data ++ record.toMap)
     }
   }
 }
+
--- sample-example-impl/src/test/scala/com/example/impl/kafka/ConsumerSpec.scala
+++ sample-example-impl/src/test/scala/com/example/impl/kafka/ConsumerSpec.scala
@@ -1,29 +1,30 @@
 package com.example.impl.kafka
 
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.{IntegerDeserializer, IntegerSerializer}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
-
 class ConsumerSpec extends WordSpec with EmbeddedKafka with Matchers with BeforeAndAfterAll with Consumer {
 
-//  implicit val deserializer: IntegerDeserializer = new IntegerDeserializer()
-//  implicit val config = EmbeddedKafkaConfig(kafkaPort = 7000, zooKeeperPort = 7001)
-//  override val consumer = EmbeddedKafka.kafkaConsumer(config,deserializer,deserializer)
-//
-//  override def beforeAll(): Unit = {
-//    EmbeddedKafka.start()
-//  }
-//
-//  override def afterAll(): Unit = {
-//    EmbeddedKafka.stop()
-//  }
-//
-//  "Consumer" should {
-//    "get message from a topic" in {
-//      assert(consumeData(Range(1, 10000).toList ,"TestTopic"))//should === true
-//    }
-//  }
+  implicit val deserializer: IntegerDeserializer = new IntegerDeserializer()
+  implicit val serializer: IntegerSerializer = new IntegerSerializer()
+  implicit val config = EmbeddedKafkaConfig(kafkaPort = 7000, zooKeeperPort = 7001)
+  override val consumer = EmbeddedKafka.kafkaConsumer(config, deserializer, deserializer)
+  val topic = "TestTopic"
+
+  override def beforeAll(): Unit = {
+    EmbeddedKafka.start()
+    EmbeddedKafka.publishToKafka(topic, new Integer(1), new Integer(123))(config, serializer, serializer)
+  }
+
+  override def afterAll(): Unit = {
+    EmbeddedKafka.stop()
+  }
+
+  "Consumer" should {
+    "get message from a topic" in {
+      assert(consumeData(topic) == Map(new Integer(1) -> new Integer(123))) //should === true
+    }
+  }
 }
 
